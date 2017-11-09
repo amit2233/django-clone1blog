60d9c42f78db55873a559655a26175a64d0c5da0
amitnana <amittambe2233@gmail.com>
2017-11-09 13:06:00 +0530

    first

---
+++ clone1/blog/forms.py
@@ -0,0 +1,45 @@
+from django import forms
+from blog.models import Post,Comment
+from django.contrib.auth.models import User
+
+class PostForm(forms.ModelForm):
+
+
+    class Meta():
+        model = Post
+        fields=('author','title','text')
+
+
+        widgets={
+             'title':forms.TextInput(attrs={'class':'textinputclass'}),
+             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+
+
+
+
+
+class UserForm(forms.ModelForm):
+    passward = forms.CharField(widget=forms.PasswordInput())
+
+    class Meta():
+        model = User
+        fields = ('username','email','password')
+
+
+
+
+
+
+class CommentForm(forms.ModelForm):
+
+
+    class Meta():
+        model=Comment
+        fields=('author','text')
+
+
+widgets=  {
+             'author':forms.TextInput(attrs={'class':'textinputclass'}),
+             'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
+        }
